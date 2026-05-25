06ccfe2f54450b68d11ff010bc79fa37b2b57d3e
hxzsh <hsho123@outlook.de>
2026-05-26 00:27:45 +0200

    Add German translations for rank display toggle messages

---
+++ src/renderer/pages/settings/appearance_settings/rank_display_toggle/rank_display_toggle.messages.de.ts
@@ -0,0 +1,4 @@
+export const RankDisplayToggleMessages = {
+  rankDisplay: () => "Rang auf der Startseite anzeigen",
+  rankDisplayDescription: () => "Wenn aktiviert, werden dein aktueller Rang und deine Wertung auf der Startseite angezeigt.",
+};
