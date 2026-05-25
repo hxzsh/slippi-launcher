0b896175d87cc0fd397d17d7d2fd034ccf09f0c8
hxzsh <hsho123@outlook.de>
2026-05-26 00:34:17 +0200

    Update default chat messages in German

---
+++ src/renderer/pages/settings/chat_settings/default_chat_messages.de.ts
@@ -0,0 +1,21 @@
+export const defaultMessages = [
+  "ggs",
+  "Eins noch",
+  "Bin gleich wieder da",
+  "Viel Glück",
+
+  "Gut gespielt",
+  "Das hat Spaß gemacht",
+  "Danke",
+  "Zu gut",
+
+  "Sorry",
+  "my b",
+  "lol",
+  "wow",
+
+  "Muss los",
+  "Gib mir 'ne Sekunde",
+  "Lass uns später nochmal spielen",
+  "Schlechte Verbindung",
+];
